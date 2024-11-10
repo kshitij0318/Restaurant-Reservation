0493a89adfa2f0b0e8350399212927869c867784
kshitij0318 <kshitijgur@gmail.com>
2024-11-10 21:50:53 +0530

    changes

--- Backend/app.js
+++ Backend/app.js
@@ -7,13 +7,14 @@ import reservationRouter from './routes/reservationrRoute.js';
 const app = express();
 dotenv.config({path : './config/config.env'});
 
-app.use(
-    cors({
-        origin: "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true,
-    })
-)
+// app.use(
+//     cors({
+//         origin: "*",
+//         methods: ["GET", "POST", "PUT", "DELETE"],
+//         credentials: true,
+//     })
+// )
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
