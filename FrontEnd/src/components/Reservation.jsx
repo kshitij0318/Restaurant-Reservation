4fc2b8127c7a7cb7ea4244db0fd4fb1719d01018
kshitij0318 <kshitijgur@gmail.com>
2024-11-11 01:54:43 +0530

    changes

--- FrontEnd/src/components/Reservation.jsx
+++ FrontEnd/src/components/Reservation.jsx
@@ -49,7 +49,7 @@ const Reservation = () => {
         <div className="banner">
           <div className="reservation_form_box">
             <h1>MAKE A RESERVATION</h1>
-            <p>For Further Questions, Please Call</p>
+            <p>Please enter the details below to make a new reservation</p>
             <form onSubmit={handleReservation}>
               <div>
                 <input
