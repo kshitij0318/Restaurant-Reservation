4e2af7ef956d9b4c36e6b9daa034c96d1b189165
kshitij0318 <kshitijgur@gmail.com>
2024-11-11 01:58:25 +0530

    changes

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,45 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { data } from "../restApi.json";
 import { Link } from "react-scroll";
 import { GiHamburgerMenu } from "react-icons/gi";
+import "../App.css"; 
 
 const Navbar = () => {
   const [show, setShow] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <>
-      <nav>
-        <div className="logo">SYMBI EATS</div>
-        <div className={show ? "navLinks showmenu" : "navLinks"}>
-          <div className="links">
-            {data[0].navbarLinks.map((element) => (
-              <Link
-                to={element.link}
-                spy={true}
-                smooth={true}
-                duration={500}
-                key={element.id}
-              >
-                {element.title}
-              </Link>
-            ))}
-          </div>
-          {/* Change button to Link for smooth scroll */}
-          <Link
-            to="menu"
-            spy={true}
-            smooth={true}
-            duration={500}
-            className="menuBtn"
-          >
-            POPULAR DISHES
-          </Link>
+    <nav className={scrolled ? "navbar scrolled" : "navbar"}>
+      <div className="logo">SYMBI EATS</div>
+      <div className={show ? "navLinks showmenu" : "navLinks"}>
+        <div className="links">
+          {data[0].navbarLinks.map((element) => (
+            <Link
+              to={element.link}
+              spy={true}
+              smooth={true}
+              duration={500}
+              key={element.id}
+            >
+              {element.title}
+            </Link>
+          ))}
         </div>
-        <div className="hamburger" onClick={() => setShow(!show)}>
-          <GiHamburgerMenu />
-        </div>
-      </nav>
-    </>
+        <Link to="menu" spy={true} smooth={true} duration={500} className="menuBtn">
+          POPULAR DISHES
+        </Link>
+      </div>
+      <div className="hamburger" onClick={() => setShow(!show)}>
+        <GiHamburgerMenu />
+      </div>
+    </nav>
   );
 };
 
